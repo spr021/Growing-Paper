0d8fc2d67d71d27d58bfcbea007099e445adee15
spr021 <saber.pourrahimi.1999@gmail.com>
2021-12-04 03:20:49 +0330

    add date bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import DateBar from './components/date-bar/dateBar';
 import Header from './components/header/header';
+import date from "./mock-data/date.json"
 
 function App() {
   return (
     <div className="App">
       <Header />
-        
+      <DateBar date={date} />
     </div>
   );
 }
---
+++ src/components/date-bar/dateBar.js
@@ -0,0 +1,25 @@
+import "./dateBar.scss"
+
+function DateBar({ date }) {
+  const selected = (day) => {
+    if(+date.currentday === day) return "selected"
+    return ""
+  }
+
+  return (
+    <div className="data-bar">
+      <div className="month">{date.month}</div>
+      <div className="days">
+        <div className={selected(+date.firstday)}><span>ش</span><span>{+date.firstday}</span></div>
+        <div className={selected(+date.firstday + 1)}><span>ی</span><span>{+date.firstday + 1}</span></div>
+        <div className={selected(+date.firstday + 2)}><span>د</span><span>{+date.firstday + 2}</span></div>
+        <div className={selected(+date.firstday + 3)}><span>س</span><span>{+date.firstday + 3}</span></div>
+        <div className={selected(+date.firstday + 4)}><span>چ</span><span>{+date.firstday + 4}</span></div>
+        <div className={selected(+date.firstday + 5)}><span>پ</span><span>{+date.firstday + 5}</span></div>
+        <div className={selected(+date.firstday + 6)}><span>ج</span><span>{+date.firstday + 6}</span></div>
+      </div>
+    </div>
+  )
+}
+
+export default DateBar
