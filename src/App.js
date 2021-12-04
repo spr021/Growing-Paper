f9e8041ec8b9cb9bd9e59c45cc7bdc74ba46a846
spr021 <saber.pourrahimi.1999@gmail.com>
2021-12-04 03:39:07 +0330

    add leaves button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import './App.css';
-import DateBar from './components/date-bar/dateBar';
-import Header from './components/header/header';
+import "./App.css"
+import DateBar from "./components/date-bar/dateBar"
+import Header from "./components/header/header"
+import Button from "./components/button/button"
+import Leaves from "./assets/Leaves.png"
 import date from "./mock-data/date.json"
 
 function App() {
@@ -8,8 +10,16 @@ function App() {
     <div className="App">
       <Header />
       <DateBar date={date} />
+      <div className="leaves">
+        <Button
+          color="#283b1c"
+          text="300"
+          className="leaves-point"
+          icon={Leaves}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
